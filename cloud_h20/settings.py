942e2f72d46a5641b848fcb7595bf62406664dc4
serge <sergerivera@upcapes.org>
2023-06-02 21:02:35 +0800

    fixed deployment

--- cloud_h20/settings.py
+++ cloud_h20/settings.py
@@ -153,7 +153,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(os.environ.get('REDIS_URL', '127.0.0.1'), os.environ.get('REDIS_PORT', '6379'))],
+            "hosts": [(os.environ.get('REDIS_URL', '127.0.0.1'))],
         },
     },
 }
