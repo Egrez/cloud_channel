aa5a08c1f279f7058e68dc1686e6628e82f8017d
serge <sergerivera@upcapes.org>
2023-06-02 19:47:55 +0800

    deployment fixes

--- cloud_h20/asgi.py
+++ cloud_h20/asgi.py
@@ -29,25 +29,3 @@ application = ProtocolTypeRouter({
         )
     ,
 })
-
-import os
-from django.conf.urls import url
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mc_backend.settings')
-
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-django_asgi_app = get_asgi_application()
-
-from channels.auth import AuthMiddlewareStack   
-import api_backend.routing
-
-
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            api_backend.routing.websocket_urlpatterns
-        )
-    )
-})
